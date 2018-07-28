5a7e08ef8cdb9f487929aef3a7cb988e68e69c57
Jonesy167 <30509908+Jonesy167@users.noreply.github.com>
2018-07-28 21:11:34 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,23 +1,40 @@
 #!/bin/bash 
 
 ##############by JONESY167 https://github.com/Jonesy167###############################
-
-if [ "$EUID" -ne 0 ]
-  then echo "needs root privileges to run"
+if [[ $EUID -ne 0 ]]; then
+   echo " "
+   echo "MEEDS ROOT PRIVILEGES TO RUN"
+   echo " "
+   sleep 1
+   sudo echo " "
 fi
 
+
+echo ' ' 
 echo GETTING UPDATE
+echo ' '
 sleep 2
 sudo -S apt-get update
+echo ' '
 echo PERFORMING DISTRIBUTION UPGRADE
+echo ' '
 sleep 2
 sudo -S apt-get -y dist-upgrade
+echo ' '
 echo FIXING MISSING DEPENDENCIES
+echo ' '
 sleep 2
 sudo apt-get install -fy
+echo ' '
 echo REMOVING OBSOLETE PACKAGES
+echo ' '
 sleep 2
 sudo -S apt-get autoremove -fy
+echo ' '
 echo CLEARING CACHE
+echo ' '
 sleep 2
 sudo -S apt-get autoclean
+echo ' '
+echo FINISHED, HAVE A NICE DAY
+echo ' '
